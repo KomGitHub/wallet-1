f1b044ebac290343e3a47dcd81d4f164d6c731a2
Komil <komil.sherzamonov@gmail.com>
2021-04-10 12:38:52 +0500

    Reject method added

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -25,3 +25,31 @@ func TestService_RegisterAccount_fail(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 }
+
+func TestService_Reject_success(t *testing.T) {
+	svc := &Service{}
+	svc.RegisterAccount("+992000000001")
+	svc.Deposit(1, 20)
+	expected_payment, expected_error := svc.Pay(1, 10, "food")
+	result_payment, result_error := svc.FindPaymentByID(expected_payment.ID)
+
+	if !reflect.DeepEqual(expected_payment, result_payment) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected_payment, result_payment)
+	}
+
+	if !reflect.DeepEqual(expected_error, result_error) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected_error, result_error)
+	}
+}
+
+func TestService_Reject_fail(t *testing.T) {
+	svc := &Service{}
+	svc.RegisterAccount("+992000000001")
+	svc.Deposit(1, 20)
+	expected_error := ErrPaymentNotFound
+	_, result_error := svc.FindPaymentByID("second")
+
+	if !reflect.DeepEqual(expected_error, result_error) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected_error, result_error)
+	}
+}
